f363b76569fea402b2cfb41d9237ae1dd4d23d90
Dave Niewinski <davesarmoury@gmail.com>
2023-08-08 17:06:59 -0400

    Roughly implemented vel and yaw.  Not sure if they will feel right yet

--- reins.py
+++ reins.py
@@ -10,21 +10,25 @@ left_relaxed = 1122
 left_pulled = 4206
 
 i2c = busio.I2C(board.SCL, board.SDA)
-#i2c = busio.I2C(board.SCL_1, board.SDA_1)
+
 ads = ADS.ADS1115(i2c)
 right = AnalogIn(ads, ADS.P0)
 left = AnalogIn(ads, ADS.P1)
 
-def clamp(val, min_val=0.0, max_val=1.0):
-  return max(min_val, min(val, max_val))
-
 def flip(in_val):
   return clamp ( ( in_val - 1.0 ) * -1.0 )
 
+def clamp(val, min_val=0.0, max_val=1.0):
+  return max(min_val, min(val, max_val))
+
 for i in range(1000):
-  r_val = (right.value - right_relaxed) / (right_pulled - right_relaxed)
-  l_val = (left.value - left_relaxed) / (left_pulled - left_relaxed)
-  print(flip(l_val), flip(r_val))
-  time.sleep(0.5)
+  r_val = flip( (right.value - right_relaxed) / (right_pulled - right_relaxed) )
+  l_val = flip( (left.value - left_relaxed) / (left_pulled - left_relaxed) )
+
+  yaw_scale = r_val - l_val
+  x_scale = min(r_val, l_val)
+
+  print(x_scale, yaw_scale)
+  time.sleep(0.1)
 
 print("done!")
